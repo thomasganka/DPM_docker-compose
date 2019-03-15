ab11e6f30f31ca92f157a6f4d2e2105dc131bcdc
antonin bruneau <antonin.bruneau@gmail.com>
2019-03-15 12:53:11 +0100

    Update sdc to 3.8

--- setup.sh
+++ setup.sh
@@ -6,12 +6,19 @@ sudo update-rc.d haveged defaults
 
 echo Install docker
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce=18.03.1~ce~3-0~ubuntu
-sudo systemctl status docker
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ${USER}
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
