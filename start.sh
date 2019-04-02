18fa940ed31d601c6d8f93248a922edffcc2378e
antonin bruneau <antonin.bruneau@gmail.com>
2019-04-02 17:33:11 +0200

    Add data transformer

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export HOSTNAME=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
 docker-compose up -d
 
-declare -a nodes=("sdc.cluster" "dpm.cluster")
+declare -a nodes=("sdc.cluster" "dpm.cluster" "sdt.cluster")
 processfile=/etc/hosts
 tmpfile=$processfile.tmp
 cp $processfile $tmpfile
