528f0af2a1e79dacf8a5be97ed12072ef10f089c
antonin bruneau <antonin.bruneau@gmail.com>
2019-07-12 09:29:42 +0200

    Add backup

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./utils/docker_volume_backup.sh ./docker-compose.yaml sch $(pwd)/backup/$(date '+%d-%b-%Y_%H-%M-%S')
+
 docker-compose down
 
 docker run -d --rm -v sch_dpm:/tmp/dpm --entrypoint "/bin/chown"  centos:7 -R 1000:1000 /tmp/dpm
